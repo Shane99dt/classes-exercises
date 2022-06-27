9180cd360879f300a3eccf9273f50a4d0b65f34d
Shane99dt <dushen1999thilaksha@gmail.com>
2022-06-27 23:48:53 +0200

    classes vid game

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ class Player{
     this.runs = 0
     this.ballsFaced = 0
     this.notOuts = 0
+    this.wickets = 0
+    this.oversBowled = 0
+    this.runsGiven = 0
+    this.maidens = 0
   }
-
-  matches = [matchNum]
 
   match = (matchNum,opponetTeam, result) => {
     // this.matchNum = matchNum
     // this.opponetTeam = opponetTeam
     // this.result = result
     this.matchesPlayed += 1
-    matches.push()
-
   }
 
   battingStats = (matchNum, runs, ballsFaced, status = false) => {
@@ -31,13 +31,27 @@ class Player{
     }
   }
 
-  bowlingStats = (matchNum, wickets, oversBowled, maidens) => {
+  bowlingStats = (matchNum, wickets, oversBowled, runsGiven, maidens) => {
     this.wickets += wickets
     this.oversBowled += oversBowled
+    this.runsGiven += runsGiven
     this.maidens += maidens
   }
-
 }
+
+// class Batting extends Player {
+//   constructor(playerName){
+//     super(playerName)
+//   }
+
+//   battingStats = (matchNum, runs, ballsFaced, status = false) => {
+//     this.runs += runs
+//     this.ballsFaced += ballsFaced
+//     if(status){
+//       this.notOuts += 1
+//     }
+//   }
+// }
 
 
 const shane = new Player("shane", 22, 'Batting All rounder')
@@ -45,9 +59,12 @@ console.log(shane)
 
 shane.match(1, 'VSK', 'win')
 shane.battingStats(1, 40, 35, true)
+shane.bowlingStats(1, 2, 4, 15, 1)
 console.log(shane)
 // shane.battingStats(2, 10, 45, false)
 
 shane.match(2, 'CCSBB', 'win')
 shane.battingStats(2, 30, 50, false)
+shane.bowlingStats(1, 0, 4, 20, 1)
 console.log(shane)
+
