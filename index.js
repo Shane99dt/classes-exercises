831aea75bf48689e0f2a192d8f83506467adf1f4
Shane99dt <dushen1999thilaksha@gmail.com>
2022-06-27 16:12:58 +0200

    classes exercise: vid game upgrades

--- index.js
+++ index.js
@@ -9,20 +9,32 @@ class Player{
     this.matchesPlayed = 0
     this.runs = 0
     this.ballsFaced = 0
+    this.notOuts = 0
   }
 
+  matches = [matchNum]
 
-  match(matchNum,opponetTeam, result){
+  match = (matchNum,opponetTeam, result) => {
     // this.matchNum = matchNum
     // this.opponetTeam = opponetTeam
     // this.result = result
     this.matchesPlayed += 1
+    matches.push()
+
   }
 
-  battingStats(matchNum, runs, ballsFaced, status){
+  battingStats = (matchNum, runs, ballsFaced, status = false) => {
     this.runs += runs
     this.ballsFaced += ballsFaced
-    // this.status = status
+    if(status){
+      this.notOuts += 1
+    }
+  }
+
+  bowlingStats = (matchNum, wickets, oversBowled, maidens) => {
+    this.wickets += wickets
+    this.oversBowled += oversBowled
+    this.maidens += maidens
   }
 
 }
@@ -32,7 +44,10 @@ const shane = new Player("shane", 22, 'Batting All rounder')
 console.log(shane)
 
 shane.match(1, 'VSK', 'win')
-shane.battingStats(1, 40, 35, false)
+shane.battingStats(1, 40, 35, true)
 console.log(shane)
-shane.battingStats(2, 10, 45, false)
+// shane.battingStats(2, 10, 45, false)
+
+shane.match(2, 'CCSBB', 'win')
+shane.battingStats(2, 30, 50, false)
 console.log(shane)
